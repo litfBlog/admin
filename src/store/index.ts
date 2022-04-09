b7ae73837f729a7ac84b9c17f73cf422d92d31c0
Litfa <admin@litf.com.cn>
2022-04-09 19:13:33 +0800

    feat(store): pinia

---
+++ src/store/index.ts
@@ -0,0 +1,23 @@
+/*
+ * @Author: litfa
+ * @Date: 2022-04-09 18:48:19
+ * @LastEditTime: 2022-04-09 19:08:23
+ * @LastEditors: litfa
+ * @Description: store
+ * @FilePath: /admin/src/store/index.ts
+ * 
+ */
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', {
+  state: () => {
+    return { count: 0 }
+  },
+  // could also be defined as
+  // state: () => ({ count: 0 })
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
+})
